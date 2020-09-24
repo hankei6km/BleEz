fd819816b4ae3eb52dfe923e03e9eef11841ca68
hankei6km <hankei6km+git@gmail.com>
2020-09-24 10:00:23 +0900

    Set MuiTheme components to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,21 @@
 import * as React from "react";
 import { render } from "react-dom";
 
+import { ThemeProvider, createMuiTheme, CssBaseline } from "@material-ui/core";
+
 import App from "./App";
 
+const theme = createMuiTheme({
+  // palette: {
+  //   type: "dark"
+  // }
+});
+
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  rootElement
+);
